4a2193bbaeb328b43f56cd9b19b26f17c36d67ee
Ryan Maugin <ryanmauginn@gmail.com>
2017-11-25 18:59:36 +0000

    Added body parser to reciever for getting SMS properties

--- src/reciever.js
+++ src/reciever.js
@@ -6,15 +6,25 @@
 //
 
 // Import all the needed modules
-const http = require('http');
+const http = require('http')
+const bodyParser = require('body-parser');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 // Initialise the express app
 const app = express()
 
+// Middleware
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json());
+app.use(bodyParser());
+
 // POST request to {server_name}/sms to return a message
 app.post('/sms', (req, res) => {
+
+	console.log(req.body);
+
+	// Create a response
 	const twiml = new MessagingResponse();
 	twiml.message('RESPONSE SENT FROM BOT!');
 	res.writeHead(200, {'Content-Type': 'text/xml'});
