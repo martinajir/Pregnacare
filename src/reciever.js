7efd6e29d093f5f6abfd912b007015a57fb9aab0
Ryan Maugin <ryanmauginn@gmail.com>
2017-11-25 16:06:55 +0000

    Added message responding functionality (reciever.js)

--- src/reciever.js
+++ src/reciever.js
@@ -5,4 +5,22 @@
 //  Created by Team Pregnacare
 //
 
+// Import all the needed modules
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
+// Initialise the express app
+const app = express()
+
+// POST request to {server_name}/sms to return a message
+app.post('/sms', (req, res) => {
+	const twiml = new MessagingResponse();
+	twiml.message('RESPONSE SENT FROM BOT!');
+	res.writeHead(200, {'Content-Type': 'text/xml'});
+	res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+	console.log("Server is listening on port 1337");
+});
